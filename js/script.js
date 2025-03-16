494494e5926504463444c10100143cbc3495339b
Ebtesam Ahmed <reachebtesam@gmail.com>
2025-03-16 05:14:46 +0500

    Updated js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ import menuHamburguer from "./module/menuHamburguer.js";
 import initGallery from "./module/gallery.js";
 
 menuHamburguer();
-initGallery();
+
+if (typeof initGallery === "function") {
+    initGallery();
+} else {
+    console.error("initGallery is not defined or not a function");
+}
+
 new SimpleAnime();
 
 document.addEventListener("contextmenu", function (e) {
